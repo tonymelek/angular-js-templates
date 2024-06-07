ac925d5888f962ae9e47bf2ce8be7ec95afb7f07
Tony Melek <tonymelek.au@gmail.com>
2024-06-07 17:12:28 +1000

    first commit

--- app/app.config.js
+++ app/app.config.js
@@ -1,23 +1,25 @@
-import home from './views/home.html'
-import about from './views/about.html'
-import contact from './views/contact.html'
-import 'angular-route';
+import home from './views/home.html';
+import about from './views/about.html';
+import contact from './views/contact.html';
+import '@uirouter/angularjs';
 
-angular.module('myApp',['ngRoute']).config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when('/', {
+angular.module('myApp', ['ui.router']).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/',
             template: home,
             controller: 'HomeController'
         })
-        .when('/about', {
+        .state('about', {
+            url: '/about',
             template: about,
             controller: 'AboutController'
         })
-        .when('/contact', {
+        .state('contact', {
+            url: '/contact',
             template: contact,
             controller: 'ContactController'
-        })
-        .otherwise({
-            redirectTo: '/'
         });
+    
+    $urlRouterProvider.otherwise('/');
 }]);
